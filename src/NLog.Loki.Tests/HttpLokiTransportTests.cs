bb787c2987b6a26bc1911616ded56fe0d9779ffc
Corentin Altepe <corentin.altepe@gmail.com>
2022-01-16 18:25:29 +0100

    Added unit testing on the ordering of the serialized logs sent to Loki.

--- src/NLog.Loki.Tests/HttpLokiTransportTests.cs
+++ src/NLog.Loki.Tests/HttpLokiTransportTests.cs
@@ -19,14 +19,14 @@ public class HttpLokiTransportTests
             new(new LokiLabels(new LokiLabel("env", "unittest"), new LokiLabel("job", "Job1")),
                 date, "Info|Receive message from A with destination B."),
             new(new LokiLabels(new LokiLabel("env", "unittest"), new LokiLabel("job", "Job1")),
-                date, "Info|Another event has occured here."),
+                date + TimeSpan.FromSeconds(2.2), "Info|Another event has occured here."),
             new(new LokiLabels(new LokiLabel("env", "unittest"), new LokiLabel("job", "Job1")),
-                date, "Info|Event from another stream."),
+                date - TimeSpan.FromSeconds(0.9), "Info|Event from another stream."),
         };
     }
 
     [Test]
-    public async Task SerializeMessageToHttpLoki()
+    public async Task SerializeMessageToHttpLokiWithoutOrdering()
     {
         // Prepare the events to be sent to loki
         var events = CreateLokiEvents();
@@ -43,12 +43,39 @@ public class HttpLokiTransportTests
             });
 
         // Send the logging request
-        var transport = new HttpLokiTransport(httpClient.Object, false);
+        var transport = new HttpLokiTransport(httpClient.Object, orderWrites: false);
         await transport.WriteLogEventsAsync(events).ConfigureAwait(false);
 
         // Verify the json message format
         Assert.AreEqual(
-            "{\"streams\":[{\"stream\":{\"env\":\"unittest\",\"job\":\"Job1\"},\"values\":[[\"1640598506000000000\",\"Info|Receive message from A with destination B.\"],[\"1640598506000000000\",\"Info|Another event has occured here.\"],[\"1640598506000000000\",\"Info|Event from another stream.\"]]}]}",
+            "{\"streams\":[{\"stream\":{\"env\":\"unittest\",\"job\":\"Job1\"},\"values\":[[\"1640598506000000000\",\"Info|Receive message from A with destination B.\"],[\"1640598508200000000\",\"Info|Another event has occured here.\"],[\"1640598505100000000\",\"Info|Event from another stream.\"]]}]}",
+            serializedJsonMessage);
+    }
+
+    [Test]
+    public async Task SerializeMessageToHttpLokiWithOrdering()
+    {
+        // Prepare the events to be sent to loki
+        var events = CreateLokiEvents();
+
+        // Configure the ILokiHttpClient such that we intercept the JSON content and simulate an OK response from Loki.
+        string serializedJsonMessage = null;
+        var httpClient = new Mock<ILokiHttpClient>();
+        _ = httpClient.Setup(c => c.PostAsync("loki/api/v1/push", It.IsAny<HttpContent>()))
+            .Returns<string, HttpContent>(async (s, json) =>
+            {
+                // Intercept the json content so that we can verify it.
+                serializedJsonMessage = await json.ReadAsStringAsync().ConfigureAwait(false);
+                return new HttpResponseMessage(HttpStatusCode.OK);
+            });
+
+        // Send the logging request
+        var transport = new HttpLokiTransport(httpClient.Object, orderWrites: true);
+        await transport.WriteLogEventsAsync(events).ConfigureAwait(false);
+
+        // Verify the json message format
+        Assert.AreEqual(
+            "{\"streams\":[{\"stream\":{\"env\":\"unittest\",\"job\":\"Job1\"},\"values\":[[\"1640598505100000000\",\"Info|Event from another stream.\"],[\"1640598506000000000\",\"Info|Receive message from A with destination B.\"],[\"1640598508200000000\",\"Info|Another event has occured here.\"]]}]}",
             serializedJsonMessage);
     }
 
@@ -75,7 +102,7 @@ public class HttpLokiTransportTests
 
         // Verify the json message format
         Assert.AreEqual(
-                    "{\"streams\":[{\"stream\":{\"env\":\"unittest\",\"job\":\"Job1\"},\"values\":[[\"1640598506000000000\",\"Info|Event from another stream.\"]]}]}",
+                    "{\"streams\":[{\"stream\":{\"env\":\"unittest\",\"job\":\"Job1\"},\"values\":[[\"1640598505100000000\",\"Info|Event from another stream.\"]]}]}",
                     serializedJsonMessage);
     }
 
