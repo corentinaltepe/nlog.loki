1bfe55fe148319430353a9361b55f93f36e49772
Corentin Altepe <corentin.altepe@gmail.com>
2022-01-24 07:47:04 +0100

    Fixed assertion in test following changing the HttpRequestException having no status code associated.

--- src/NLog.Loki.Tests/HttpLokiTransportTests.cs
+++ src/NLog.Loki.Tests/HttpLokiTransportTests.cs
@@ -136,6 +136,6 @@ public class HttpLokiTransportTests
         var transport = new HttpLokiTransport(httpClient.Object, false);
         var exception = Assert.ThrowsAsync<HttpRequestException>(() => transport.WriteLogEventsAsync(CreateLokiEvents()));
         Assert.AreEqual("Failed pushing logs to Loki.", exception.Message);
-        Assert.AreEqual(HttpStatusCode.Conflict, exception.StatusCode);
+        Assert.Null(exception.StatusCode);
     }
 }
