45643cc59bc35619a65882615a885dc579d772d7
Corentin Altepe <corentin.altepe@gmail.com>
2023-08-05 15:07:53 +0200

    Added another logging message in the template project where there are more additional parameters than referenced in the message template.

--- src/Template/Program.cs
+++ src/Template/Program.cs
@@ -1,4 +1,4 @@
-﻿using NLog;
+using NLog;
 
 // Template console project running NLog and NLog.Loki
 var logger = LogManager.GetCurrentClassLogger();
@@ -7,7 +7,9 @@ logger.Info("Starting Template application.");
 logger.Debug("Some low level info {A}, {B}, {C}.", "Hello", 12, true);
 
 int i = 0;
-while(true) {
+while(true)
+{
     logger.Info("Doing some hard work... Iteration {I}.", i++);
+    logger.Info("Some additional information {Info1}", "Piece of info 1", new { arg1 = "info2", value = 22 });
     await Task.Delay(1000);
 }
